b500775cc52de3d2d2ad4794ef185ea1e9fdaaaf
rahatil4769 <rahaman15-5129@diu.edu.bd>
2025-04-13 23:58:56 +0600

    color changing

--- lib/theme_color.dart
+++ lib/theme_color.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color.fromARGB(255, 57, 100, 137);
   static const Color secondary = Color.fromARGB(255, 123, 97, 32);
-  static const Color accent = Color.fromARGB(255, 49, 35, 210);
+  static const Color accent = Color.fromARGB(255, 156, 243, 109);
 
-  static const Color background = Color(0xFFF5F5F5);
-  static const Color surface = Color(0xFFFFFFFF);
+  static const Color background = Color.fromARGB(255, 230, 224, 224);
+  static const Color surface = Color.fromARGB(255, 0, 0, 0);
 
   static const Color success = Color(0xFF43A047);
   static const Color warning = Color(0xFFFFA726);

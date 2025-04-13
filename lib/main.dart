ff449736132be3e8149a3e6555c5ce6f1ab82c82
rahatil4769 <rahaman15-5129@diu.edu.bd>
2025-04-13 23:55:00 +0600

    Clean up ignored files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'task_8.dart';
+import 'package:flutter_practice/task_1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,33 +12,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Roboto',
-      ),
-    
-     //home: const Task_1(),
-     //home: const Task_2(),
-    // home: const Task_3(),
-    // home: const Task_4(),
-    // home: const Task_5(),
-    // home: const Task_6(),
-    // home: const Task_7(),
-    // home: const Task_8(),
-    // home: const Task_9(),
-    // home: const Task_10(
-    // home: const Task_11(),
-    // home: const Task_12(),
-    // home: const Task_13(),
-    // home: const Task_14(),
-    //  home: const Task_15(),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Roboto'),
+
+      home: const Task_1(),
+
+      //home: const Task_2(),
+      // home: const Task_3(),
+      // home: const Task_4(),
+      // home: const Task_5(),
+      // home: const Task_6(),
+      // home: const Task_7(),
+      // home: const Task_8(),
+      // home: const Task_9(),
+      // home: const Task_10(
+      // home: const Task_11(),
+      // home: const Task_12(),
+      // home: const Task_13(),
+      // home: const Task_14(),
+      //  home: const Task_15(),
       // home: const Task_16(),
       // home: const Task_17(),
       //  home: const Task_18(),
       // home: const Task_19(),
-       home: const Task_8(),
-      
-      
+      //  home: const Task_8(),
     );
   }
 }
